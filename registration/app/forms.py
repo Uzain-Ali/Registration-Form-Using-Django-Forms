3837cd34f064e11ac1e1d8a58c1b4c56450d6997
Uzain95 <uzainali95@gmail.com>
2024-06-01 03:22:55 -0700

    Updation

--- registration/app/forms.py
+++ registration/app/forms.py
@@ -1,8 +1,18 @@
 from django import forms 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model=User
         fields= ('username','email', 'first_name','last_name','password1', 'password2')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField( max_length=100)
+    password = forms.CharField( max_length=100)
+
+    if username and password:
+        user = authenticate( username=username, password=password)
+        user.save()
